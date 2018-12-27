0c499d43ba708b3cbf724b186b3ad81f460ad2f4
adrianabuelvas298 <42812861+adrianabuelvas298@users.noreply.github.com>
2018-12-27 18:53:10 -0500

    valores

---
+++ MyApplication/app/src/main/java/com/example/adriana/myapplication/Registrar.java
@@ -0,0 +1,14 @@
+package com.example.adriana.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Registrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_registrar);
+    }
+
+}
